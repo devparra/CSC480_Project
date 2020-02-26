6ede31f16dd69d8b9177dbba5abd0aa304f65bfb
Brian Hurst <42877196+hurstbsmath@users.noreply.github.com>
2020-02-26 15:04:17 -0600

    The final product?

--- HOA/app/src/main/java/com/CS480/hoa/ConvertImage.java
+++ HOA/app/src/main/java/com/CS480/hoa/ConvertImage.java
@@ -1,10 +1,15 @@
 package com.CS480.hoa;
 
 import android.graphics.Bitmap;
-import android.os.AsyncTask;
+import android.os.FileUtils;
 import android.util.Base64;
+import android.util.Base64OutputStream;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class ConvertImage {
@@ -12,8 +17,42 @@ public class ConvertImage {
 
     public static String convertImageToString(Bitmap image){
 
-       return encodeToBase64(image, Bitmap.CompressFormat.JPEG, 80);
+       return encodeToBase64(image, Bitmap.CompressFormat.JPEG, 70);
 
+    }
+
+
+    public static String convertFileToString(File file){
+
+        return encodeToBase64(file);
+    }
+
+
+
+    private static String encodeToBase64(File file){
+
+        try {
+
+            FileInputStream fileInputStream = new FileInputStream(file);
+            ByteArrayOutputStream byteArrayOS = new ByteArrayOutputStream();
+
+            int nextByte = fileInputStream.read();
+            while(nextByte != -1){
+                byteArrayOS.write(nextByte);
+                nextByte = fileInputStream.read();
+            }
+
+            String base64String = Base64.encodeToString(byteArrayOS.toByteArray(), Base64.NO_WRAP);
+
+
+            return base64String;
+
+        }catch(IOException error) {
+            System.out.println("Error in converting file*************************************************");
+            System.out.println(error.getMessage());
+        }
+
+        return null;
     }
 
 
